f6ff0ac39a90842f842cded22967f9afdb036412
kotsukod <kotsukod@gmail.com>
2022-09-15 17:24:28 +0300

    Задача № 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int Max = numA;
+if (numB > Max)
+{
+    Max = numB;
+}
+if (numC > Max)
+{
+    Max = numC;
+}
+Console.WriteLine("Max = " + Max);
