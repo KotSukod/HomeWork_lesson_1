616852bd128f266d177235661488b358c382d7d0
kotsukod <kotsukod@gmail.com>
2022-09-15 17:43:42 +0300

    Задача № 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+int result;
+while (count < (num + 1))
+{
+    result = count % 2;
+    if (result == 0)
+    {
+        Console.Write(count + ", ");
+    }
+    count += 1;
+}
